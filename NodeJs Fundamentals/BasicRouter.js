51bf269638a2e7de49eb182ae813ede7938c769e
jmnjmnjmn <jiamengnan@gmail.com>
2016-04-11 13:44:24 -0700

    post

--- NodeJs Fundamentals/BasicRouter.js
+++ NodeJs Fundamentals/BasicRouter.js
@@ -1,11 +1,11 @@
 'use strict';
 const http = require('http');
 const url = require('url');
+const qs = require('querystring');
 
 let routes = {
 	'GET': {
 		'/': (req, res) => {
-			
 			res.writeHead(200, {'Content-type': 'text/html'});
 			res.end('<h1>Hello Router</h1>');
 		},
@@ -15,13 +15,32 @@ let routes = {
 		},
 		'/api/getinfo': (req, res) => {
 			// fetch data from db and respond as JSON
-
 			res.writeHead(200, {'Content-type': 'application/json'});
 			res.end(JSON.stringify(req.queryParams));
 		}
 	},
 	'POST': {
-		
+		'/api/login': (req, res) => {
+			let body = '';
+			req.on('data', data => {
+				body += data;
+				console.log('Size: ', body.length / (1024 * 1024));
+				if(body.length > 2097152) {
+					res.writeHead(413, {'Content-type': 'text/html'});
+					res.end('<h3>Error: The file being uploaded exceeds the 2MB limit</h3>');
+					req.connection.destroy();
+				}
+			});
+
+			req.on('end', () => {
+				let params = qs.parse(body);
+				console.log('Username: ', params['username']);
+				console.log('Password: ', params['password']);
+				// Query a db to see if the user exists
+				// If so, send a JSON response to the SPA
+				res.end();
+			});
+		}
 	},
 	'NA': (req, res) => {
 		res.writeHead(404);
@@ -31,7 +50,7 @@ let routes = {
 
 function router(req, res) {
 	let baseURI = url.parse(req.url, true);
-	console.log('Requested route: ',baseURI);
+	console.log('url: ',baseURI);
 	let resolveRoute = routes[req.method][baseURI.pathname];
 	if(resolveRoute != undefined) {
 		req.queryParams = baseURI.query;
