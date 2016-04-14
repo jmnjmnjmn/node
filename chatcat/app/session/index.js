2155a9099e84f8101888fadece577c9f2f85deba
jmnjmnjmn <jiamengnan@gmail.com>
2016-04-14 09:00:13 -0700

    fix bugs

--- chatcat/app/session/index.js
+++ chatcat/app/session/index.js
@@ -2,6 +2,7 @@
 const session =  require('express-session');
 const MongoStore = require('connect-mongo')(session);
 const config = require('../config');
+const db = require('../db');
 
 if(process.env.NODE_ENV === "production"){
 	module.exports = session({
